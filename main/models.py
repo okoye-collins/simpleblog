bf0a10ed58d8eda975bfc91008b6dc5ec22efe2d
okoye_collins <collins.kcj@gmail.com>
2024-02-29 20:37:43 +0100

    add bootstrap

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=225)
+    tag = models.CharField(max_length=225, default="")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
 
